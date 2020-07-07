178f40e6b5a965cef36919ff707d89d6cf2d26aa
Julio Lopez <ljuliom@gmail.com>
2020-07-07 16:33:41 -0500

    Styles for Feedback

--- components/Card.js
+++ components/Card.js
@@ -1,18 +1,22 @@
 import { useState } from 'react';
 import detectNetwork from '../lib/cards.js';
 
-export default function NewCard(props){
-    const [cardType, setCardType] = useState();
+export default function NewCard(){
+    const [state, setState] = useState({});
 
     function handleChange(e){
-        setCardType(detectNetwork(e.target.value));
+        let cardType = detectNetwork(e.target.value);
+        setState({
+            cardType: cardType,
+            status: ((cardType == "Invalid Card") ? "error" : "ok")
+        })
     }
 
     return (
         <div>
             <h3>Insert your card number:</h3>
-            <input id="card-number" type="text" onChange={handleChange} />
-            <span id="card-type">{cardType}</span>
+            <input id="card-number" type="text" onChange={handleChange} placeholder="Type a Card Number..."/>
+            <span className={state.status} id="card-type">{state.cardType}</span>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import Card from '../components/Card';
-import detectNetwork from '../lib/cards';
 
 export default function Home(){
   return (
-    <div>
+    <main>
       <h1>Card Networks</h1>
       <Card/>
-    </div>
+    </main>
   )
 }
